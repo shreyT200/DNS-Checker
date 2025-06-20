1a3ab5100c8043f89f04d693902c95f65e52e1e3
shreyT200 <shrey9202@gmail.com>
2025-06-20 12:20:32 +0530

    1

--- client/src/components/Propagation.jsx
+++ client/src/components/Propagation.jsx
@@ -200,7 +200,7 @@ sx={{
   flexDirection:'column',
   margin:"0 auto",
   justifyContent:'center',
-  alignTtems: 'center',
+  alignItems: 'center',
   borderRadius:'5px',
   boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
 }}>
